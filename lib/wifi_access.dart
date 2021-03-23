fe6843180f680b01b9aa18194796c3eef8252d1a
attex <f>
2021-03-23 17:40:27 +0100

    Fixed Type

--- lib/wifi_access.dart
+++ lib/wifi_access.dart
@@ -26,7 +26,7 @@ class WifiAccess {
   static const MethodChannel _channel = const MethodChannel('wifi_access');
 
   static Future<DHCP> get dhcp async {
-    return DHCP.fromMap(Map<String, String>.from(await (_channel
-        .invokeMethod('getDHCP') as FutureOr<Map<dynamic, dynamic>>)));
+    return DHCP.fromMap(
+        Map<String, String>.from(await _channel.invokeMethod('getDHCP')));
   }
 }
